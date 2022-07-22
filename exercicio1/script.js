5e1b4d26a7801fbc7f6bedd194081d07aebfeacc
Raul Cesar <raulcesar.rcf@gmail.com>
2022-07-21 21:10:03 -0300

    atividades da labenu

---
+++ exercicio1/script.js
@@ -0,0 +1,7 @@
+var num = parseInt(prompt("Insira um numero par:   "))
+console.log(typeof num)
+console.log(num%2)
+
+//o valor retornado em par é 0
+
+//o valor retornado em impar é 1
---
+++ exercicio3/script.js
@@ -0,0 +1,18 @@
+
+const operacao1 = 5 > 20 && 5 < 2;
+const operacao2 = 5 == 5 || 5 == "5";
+const operacao3 = !20 > 50
+const operacao4 = !(20>50)
+
+
+console.log("5 > 20 && 5 < 2:    ", operacao1)
+console.log("5 == 5 || 5 == '5':   ", operacao2)
+console.log("!20 > 50:   ", operacao3)
+console.log("!(20>50):   ", operacao4)
+
+
+
+
+
+
+
